a067519a5f04150cb214cd3a5e964300f1f0daf7
flepied <flepied@gmail.com>
2002-07-08 06:24:22 +0000

    first version

---
+++ temp.pl
@@ -0,0 +1,13 @@
+#---------------------------------------------------------------
+# Project         : @PROJECT@
+# Module          : @APPLICATION@
+# File            : @FILE_NAME@
+# Version         : @VERSION_STRING@
+# Author          : @USER_FULL_NAME@
+# Created On      : @CURRENT_TIME@
+# Purpose         : 
+#---------------------------------------------------------------
+
+@DOT@
+
+# @FILE_NAME@ ends here
