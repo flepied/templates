fb52a574b830e61a99953f984996e54f47fb0af8
fred <flepied@gmail.com>
2008-04-19 18:04:42 +0000

    first version

---
+++ temp.erl
@@ -0,0 +1,15 @@
+%%===========================================================================
+%% Project         : @APPLICATION@
+%% File            : @FILE_NAME@
+%% Version         : @VERSION_STRING@
+%% Author          : @USER_FULL_NAME@
+%% Created On      : @CURRENT_TIME@
+%% Purpose         : 
+%%===========================================================================
+
+-module(@BASEFILENAME_WITHOUT_EXT@).
+-export([]).
+
+@DOT@
+
+%% @FILE_NAME@ ends here
